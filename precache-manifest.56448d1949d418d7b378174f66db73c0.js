ba714bf8b3ba8b4aa7a4546f65eb56d81c939a66
Igor <igorvm87@gmail.com>
2020-10-14 17:28:12 +0300

    Updates

---
+++ precache-manifest.56448d1949d418d7b378174f66db73c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5443cdc417794e074c98ce42524e938d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "2cbe250bfb88f9292c69",
+    "url": "/react-deploy/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbe250bfb88f9292c69",
+    "url": "/react-deploy/static/js/main.b99d7549.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "0c6a36474fecc55dd8441f495b9ad4a7",
+    "url": "/react-deploy/static/media/87.0c6a3647.JPG"
+  }
+]);
